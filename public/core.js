97e1bb7bc99bd41eddaf9e73739924f31e030865
Yamini ratna thota <KH1924@KH1924.kitspl.com>
2016-01-30 15:42:07 +0530

    toDo first app

---
+++ public/core.js
@@ -0,0 +1,35 @@
+var TodoApp = angular.module('TodoApp',[]);
+
+function mainController($scope,$http){
+	$scope.formData = {};
+	$http.get('/api/todos')
+	.success(function(data){
+		$scope.todos = data;
+		console.log(data);
+	})
+	.error(function(err){
+		console.log('Error:'+err);
+	});
+	
+	$scope.createTodo = function(){
+		$http.post('/api/todos',$scope.formData)
+		.success(function(data){
+			$scope.formData = {};
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(err){
+			console.log('Error:'+err);
+		});
+	};
+	$scope.deleteTodo = function(id){
+		$http.delete('/api/todos/'+id)
+		.success(function(data){
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(err){
+			console.log(err);
+		});
+	};
+}
